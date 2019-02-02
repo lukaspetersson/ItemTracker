14a3cb7ddb63081c4af41529ead4aa9d79bde656
lukaspetersson <lukas.petersson.1999@gmail.com>
2019-02-02 17:24:44 +0100

    Menu

--- app/src/main/java/com/lukas/android/ItemTracker/MainActivity.java
+++ app/src/main/java/com/lukas/android/ItemTracker/MainActivity.java
@@ -1,11 +1,17 @@
 package com.lukas.android.ItemTracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -14,7 +20,7 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
@@ -30,15 +36,20 @@ public class MainActivity extends FragmentActivity {
 
     DateFormat nameFormatter;
     DateFormat numberFormatter;
-
     long currentDate;
-    private final long MILI_IN_DAY = 86400000;
+
+    private final long MILIS_IN_DAY = 86400000;
+    private final int DAYS_IN_WEEK = 7;
+    private final int START_DAY = 1;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
 
         PreviousWeek = findViewById(R.id.previous_week);
         NextWeek = findViewById(R.id.next_week);
@@ -50,21 +61,14 @@ public class MainActivity extends FragmentActivity {
         numberFormatter = new SimpleDateFormat("d");
 
         setUpDateBar();
-
-        mAdapter =  new PagerAdapterMain(getSupportFragmentManager());
-        ItemsList.setAdapter(mAdapter);
-        ItemsList.setOffscreenPageLimit(7);
-        ItemsList.setCurrentItem(1);
-
-
-        setDayDisplay();
+        setUpPager();
     }
 
     private void setUpDateBar(){
-        DateNumber = new TextView[7];
-        DateName = new TextView[7];
-        DateLable = new LinearLayout[7];
-        for(int i=0; i<7; i++){
+        DateNumber = new TextView[DAYS_IN_WEEK];
+        DateName = new TextView[DAYS_IN_WEEK];
+        DateLable = new LinearLayout[DAYS_IN_WEEK];
+        for(int i=0; i<DAYS_IN_WEEK; i++){
             int numberId = getResources().getIdentifier("num" + i, "id", getPackageName());
             int nameId = getResources().getIdentifier("name" + i, "id", getPackageName());
             int labelId = getResources().getIdentifier("date" + i, "id", getPackageName());
@@ -73,8 +77,8 @@ public class MainActivity extends FragmentActivity {
             DateName[i] = findViewById(nameId);
             DateLable[i] = findViewById(labelId);
 
-            DateNumber[i].setText(numberFormatter.format(currentDate));
-            DateName[i].setText(nameFormatter.format(currentDate));
+            DateNumber[i].setText(numberFormatter.format(currentDate + MILIS_IN_DAY*(i-START_DAY)));
+            DateName[i].setText(nameFormatter.format(currentDate+ MILIS_IN_DAY*(i-START_DAY)));
 
             DateLable[i].setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -87,25 +91,62 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+    private void setUpPager(){
+        mAdapter =  new PagerAdapterMain(getSupportFragmentManager());
+        ItemsList.setAdapter(mAdapter);
+        ItemsList.setCurrentItem(START_DAY);
+        DateNumber[START_DAY].setTextColor(getResources().getColor(R.color.colorAccent));
+        DateLable[START_DAY].setBackground(ContextCompat.getDrawable(this, R.drawable.date_selected));
+        final Context context = this;
+
+        ItemsList.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            public void onPageScrollStateChanged(int state) {}
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+            public void onPageSelected(int position) {
+                for(int i=0; i<DAYS_IN_WEEK; i++){
+                    DateNumber[i].setTextColor(Color.parseColor("#ffffff"));
+                    DateLable[i].setBackground(null);
+                }
+                DateLable[position].setBackground(ContextCompat.getDrawable(context, R.drawable.date_selected));
+                DateNumber[position].setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+    }
+
     public void openScan(View view) {
 
     }
 
     public void goToNextWeek(View view) {
-        currentDate += MILI_IN_DAY;
-        setDayDisplay();
+        currentDate += MILIS_IN_DAY;
         ItemsList.setCurrentItem(ItemsList.getCurrentItem() + 1);
     }
 
     public void goToPreviousWeek(View view) {
-        currentDate -= MILI_IN_DAY;
-        setDayDisplay();
+        currentDate -= MILIS_IN_DAY;
         ItemsList.setCurrentItem(ItemsList.getCurrentItem() - 1);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        if(1==1){
+            menu.findItem(R.id.action_error).setIcon(R.drawable.error_active_24);
+        }else{
+            menu.findItem(R.id.action_error).setIcon(R.drawable.baseline_error_outline_white_24);
+        }
+        return true;
+    }
 
-    private void setDayDisplay() {
-        DateFormat formatter = new SimpleDateFormat("d/M");
-
-        //reload adapter
+    //if menu option is clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_time) {
+            ItemsList.setCurrentItem(START_DAY);
+            return true;
+        }else if (id == R.id.action_error) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
