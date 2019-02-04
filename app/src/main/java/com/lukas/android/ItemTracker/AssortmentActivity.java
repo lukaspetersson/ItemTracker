c63551b44af3b91cb72f53e4855cc936717937a2
lukaspetersson <lukas.petersson.1999@gmail.com>
2019-02-04 19:51:50 +0100

    product activity

--- app/src/main/java/com/lukas/android/ItemTracker/AssortmentActivity.java
+++ app/src/main/java/com/lukas/android/ItemTracker/AssortmentActivity.java
@@ -1,7 +1,10 @@
 package com.lukas.android.ItemTracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AssortmentActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class AssortmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assortment);
         setTitle(R.string.assortment_title);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.assortment_menu, menu);
+        return true;
+    }
+
+    //if menu option is clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add) {
+            Intent openAssortment = new Intent(AssortmentActivity.this, ProductActivity.class);
+            startActivity(openAssortment);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/lukas/android/ItemTracker/ProductActivity.java
@@ -0,0 +1,14 @@
+package com.lukas.android.ItemTracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+        setTitle(R.string.product_title);
+    }
+}
