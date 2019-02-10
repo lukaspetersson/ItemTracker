063e821491434c19bae2abf0ccc1aa9d04d1645e
lukaspetersson <lukas.petersson.1999@gmail.com>
2019-02-10 17:58:47 +0100

    make assortment adapter to a cursor adapter

--- app/src/main/java/com/lukas/android/ItemTracker/ListAdapterAssortment.java
+++ app/src/main/java/com/lukas/android/ItemTracker/ListAdapterAssortment.java
@@ -1,41 +1,45 @@
 package com.lukas.android.ItemTracker;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.lukas.android.ItemTracker.data.ItemContract;
+
 import java.util.ArrayList;
 
-public class ListAdapterAssortment extends ArrayAdapter<Product> {
+public class ListAdapterAssortment extends CursorAdapter {
 
-    public ListAdapterAssortment(Context context, ArrayList<Product> items) {
-        super(context, 0, items);
+    public ListAdapterAssortment(Context context, Cursor cursor) {
+        super(context, cursor, 0);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.assortment_layout, parent, false);
+    }
 
-        View listItemView = convertView;
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.assortment_layout, parent, false);
-        }
-
-        Product currentProduct = getItem(position);
+    @Override
+    public void bindView(View listItemView, Context context, Cursor cursor) {
 
         TextView nameView = listItemView.findViewById(R.id.assortment_name);
-        nameView.setText(currentProduct.getName());
+        int nameColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_NAME);
+        nameView.setText(cursor.getString(nameColumnIndex));
 
         TextView durabilityView = listItemView.findViewById(R.id.assortment_durability);
-        durabilityView.setText(currentProduct.getDurability()+"");
+        int durabilityColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_DURABILITY);
+        durabilityView.setText(cursor.getInt(durabilityColumnIndex) + "");
 
         TextView barcodeView = listItemView.findViewById(R.id.assortment_barcode);
-        barcodeView.setText(currentProduct.getBarcode()+"");
+        int barcodeColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_BARCODE);
+        barcodeView.setText(cursor.getLong(barcodeColumnIndex) + "");
 
-        return listItemView;
+
     }
 }
