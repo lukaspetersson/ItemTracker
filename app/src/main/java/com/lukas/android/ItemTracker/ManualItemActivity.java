0bdf08eb1a272d09226dce699b307a0859d8e8db
lukaspetersson <lukas.petersson.1999@gmail.com>
2019-02-16 20:26:02 +0100

    searchable activity removed

--- app/src/main/java/com/lukas/android/ItemTracker/ManualItemActivity.java
+++ app/src/main/java/com/lukas/android/ItemTracker/ManualItemActivity.java
@@ -20,36 +20,4 @@ public class ManualItemActivity extends AppCompatActivity {
 
     }
 
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-
-        /*if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Log.v("ManualItemActivity","HHHHHHHHHHHHHHHHjjjö"+query);
-        }*/
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the options menu from XML
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.manual_item_manu, menu);
-
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search_action).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-        searchView.setSubmitButtonEnabled(true);
-        searchView.setQueryRefinementEnabled(true);
-
-        return true;
-    }
 }
